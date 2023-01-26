3119d5690a56fe54d1b7688036df57115bd5fb74
Andrew <56186268+Andrew-Marty00shenko@users.noreply.github.com>
2023-01-26 16:29:05 +0300

    Add navigation

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import Header from "Components/Header/Header";
+import Navigation from "Components/Navigation/Navigation";
 
 const App = () => {
   return <div className="wrapper">
     <Header />
+    <Navigation />
   </div>
 }
 
